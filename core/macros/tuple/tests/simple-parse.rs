c84e1d2967ba29b27b7ecaa621f2da758c3d02f9
asungy <62207329+asungy@users.noreply.github.com>
2023-11-17 23:36:20 -0500

    Added appropriate generics in test

--- core/macros/tuple/tests/simple-parse.rs
+++ core/macros/tuple/tests/simple-parse.rs
@@ -1,7 +1,10 @@
 use tuple_derive::Tuple2;
-use tuple_trait::Tuple2;
+use tuple_trait::{ Tuple2, Numeric };
 
 #[derive(Tuple2)]
-pub struct Tuple2Thing {}
+pub struct Tuple2Thing<T: Numeric> {
+    x: T,
+    y: T,
+}
 
 fn main() {}
