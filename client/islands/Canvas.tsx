5cbd4406521eba13e00811888372833b5f6033c7
asungy <62207329+asungy@users.noreply.github.com>
2023-11-23 17:06:55 -0500

    Strobin'

--- client/islands/Canvas.tsx
+++ client/islands/Canvas.tsx
@@ -1,20 +1,39 @@
-import { useRef, useEffect } from "preact/hooks";
+import { useRef, useEffect, useState } from "preact/hooks";
+
+class Data {
+  width: number;
+  height: number;
+  toggle: boolean;
+
+  constructor() {
+    this.width = 100;
+    this.height = 100;
+    this.toggle = false;
+  }
+}
 
 export default function Canvas() {
   const canvasRef = useRef(null);
+  const [ data, setData ] = useState<Data>(new Data());
 
   useEffect(() => {
     const ctx = canvasRef.current.getContext("2d");
-    ctx.fillStyle = "#f00";
-    ctx.fillRect(0, 0, 120, 120);
-
-    const imageData = ctx.createImageData(100, 100);
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData.data[i + 3] = 255;
-    }
-    console.log(imageData);
+    const imageData = ctx.createImageData(data.width, data.height);
+    if (data.toggle) {
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        imageData.data[i + 0] = 255;
+        imageData.data[i + 3] = 255;
+      }
+    } else {
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        imageData.data[i + 1] = 255;
+        imageData.data[i + 3] = 255;
+      }
+   }
     ctx.putImageData(imageData, 0, 0);
-  }, []);
+
+    setData({...data, toggle: !data.toggle})
+  }, [data]);
 
   return (
     <>
