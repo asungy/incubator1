4ef6436cffd715c92ed01c9d150452d95fbbea6f
asungy <62207329+asungy@users.noreply.github.com>
2023-11-23 16:06:20 -0500

    Poc: modifying pixels

--- client/islands/Canvas.tsx
+++ client/islands/Canvas.tsx
@@ -7,12 +7,19 @@ export default function Canvas() {
     const ctx = canvasRef.current.getContext("2d");
     ctx.fillStyle = "#f00";
     ctx.fillRect(0, 0, 120, 120);
+
+    const imageData = ctx.createImageData(100, 100);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i + 3] = 255;
+    }
+    console.log(imageData);
+    ctx.putImageData(imageData, 0, 0);
   }, []);
 
   return (
     <>
       <h1>Hello, Canvas!</h1>
-      <canvas ref={canvasRef} />
+      <canvas style={{border: "1px solid black"}} ref={canvasRef} />
     </>
   );
 }
